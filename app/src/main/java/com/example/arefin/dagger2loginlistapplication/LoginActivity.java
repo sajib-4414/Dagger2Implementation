165118de9c15238a26b134106346c00f046e7859
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2019-02-07 23:21:17 +0600

    modifications for new package

--- app/src/main/java/com/example/arefin/dagger2loginlistapplication/LoginActivity.java
+++ app/src/main/java/com/example/arefin/dagger2loginlistapplication/LoginActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.arefin.dagger2loginlistapplication.sharedpref_injections.DaggerSharedPrefComponent;
 import com.example.arefin.dagger2loginlistapplication.sharedpref_injections.SharedPrefComponent;
 import com.example.arefin.dagger2loginlistapplication.sharedpref_injections.SharedPreferenceModule;
 
